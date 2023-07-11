6a6fae5fe951309670caa0783e79f7d53d67a7fe
yael2yoch <36213876832@mby.co.il>
2023-07-11 18:59:36 +0300

    fixing cifar100 using in main.py

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ def extract_data():
                           '../cifar-10-batches-py/data_batch_3',
                           '../cifar-10-batches-py/data_batch_4',
                           '../cifar-10-batches-py/data_batch_5',
-                         '../cifar-10-batches-py/test_batch']
+                          '../cifar-10-batches-py/test_batch']
 
-    cifar100_file_paths=['./cifar-100-python/train','./cifar-100-python/test']
+    cifar100_file_paths=['../cifar-100-python/train','../cifar-100-python/test']
 
     # labels:[b'fish', b'flowers', b'fruit_and_vegetables', b'people', b'trees']
     cifar100_labels=[1,4,2,14,17]
@@ -19,9 +19,9 @@ def extract_data():
     #extract data for cifar-10
     cifar10Extractor=Cifar10Extractor()
 
-    #read the image
+    # #read the image
     cifar10Extractor.read_all(cifar10_file_paths)
-    #write the image
+    # #write the image
     cifar10Extractor.write_images('../images')
 
     # extract data for cifar-100
@@ -29,9 +29,9 @@ def extract_data():
     cifar100Extractor=Cifar100Extractor()
 
     # read the image
-    cifar10Extractor.read_all(cifar100_file_paths)
+    cifar100Extractor.read_all(cifar100_file_paths)
     #write the image
-    cifar10Extractor.write_images('../images',cifar100_labels)
+    cifar100Extractor.write_images('../images',cifar100_labels)
 
 
 
