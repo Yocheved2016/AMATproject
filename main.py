598ed05cbd0f0c5cf13a6c088afcdf2f32519d66
tzwand <tzipwand@gmail.com>
2023-07-12 08:50:52 +0300

    'write_to_csv'

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 from Cifar10Extractor import  Cifar10Extractor
 from Cifar100Extractor import Cifar100Extractor
+from CreateCsv import create_or_add_csv
 
 
 def extract_data():
@@ -11,7 +12,7 @@ def extract_data():
                           '../cifar-10-batches-py/data_batch_5',
                          '../cifar-10-batches-py/test_batch']
 
-    cifar100_file_paths=['./cifar-100-python/train','./cifar-100-python/test']
+    cifar100_file_paths=['../cifar-100-python/train','../cifar-100-python/test']
 
     # labels:[b'fish', b'flowers', b'fruit_and_vegetables', b'people', b'trees']
     cifar100_labels=[1,4,2,14,17]
@@ -21,23 +22,27 @@ def extract_data():
 
     #read the image
     cifar10Extractor.read_all(cifar10_file_paths)
+    create_or_add_csv(cifar10Extractor.file_names,cifar10Extractor.labels,'cifar10','../cifar_data.csv')
     #write the image
-    cifar10Extractor.write_images('../images')
+    #cifar10Extractor.write_images('../images')
 
     # extract data for cifar-100
 
     cifar100Extractor=Cifar100Extractor()
 
     # read the image
-    cifar10Extractor.read_all(cifar100_file_paths)
+    cifar100Extractor.read_all(cifar100_file_paths)
+    create_or_add_csv(cifar100Extractor.file_names,cifar100Extractor.labels,'cifar100','../cifar_data.csv')
+
     #write the image
-    cifar10Extractor.write_images('../images',cifar100_labels)
+    #cifar100Extractor.write_images('../images',cifar100_labels)
 
 
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+
     extract_data()
 
 
