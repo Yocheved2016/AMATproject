114360f197a2ce73022a8cd6dd56ed2142391078
tz1448 <137272212+tz1448@users.noreply.github.com>
2023-07-20 12:41:01 +0300

    Update Preprocessing.py

--- DataArrangement/Preprocessing.py
+++ DataArrangement/Preprocessing.py
@@ -4,9 +4,7 @@ import numpy as np
 
 
 def normalize_data(data):
-    shape_data = data.shape
-    flatten_data = data.flatten()
-    normalized_data = preprocessing.normalize([flatten_data])
-    normalized_data = normalized_data.reshape(shape_data)
-
+   
+    data = data.astype('float32')
+    normalized_data = data / 255.0
     return normalized_data
